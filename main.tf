c7080d55745cfb3cfae0e30abbc2f574764117d4
venkatarao-Srinu <venkataraosrinu12@gmail.com>
2025-12-16 22:08:09 +0530

    S3 and Dynamo DB Table Lock details

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket_name
+
+  tags = {
+    Name = "terraform-remote-state"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.lock_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-state-lock"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "state_bucket_name" {
+  default = "dev-terraform-remote-state-12345"
+}
+
+variable "lock_table_name" {
+  default = "terraform-locks"
+}
